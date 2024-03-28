a0055f2306ba8dd218e750c44687a40de05b073b
Vedha <Vedha@DESKTOP-7GFC154>
2024-03-28 11:46:33 +0530

    Project2 is committed

---
+++ Project2/src/main/java/mvnpro/Sample.java
@@ -0,0 +1,15 @@
+package mvnpro;
+
+public class Sample 
+
+{
+static String name="Vedha";
+
+	public static void main(String[] args) 
+	{
+		int id=1;
+		System.out.println(name+" "+id);
+		 
+	}
+
+}
